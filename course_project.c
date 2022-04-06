ec9fe96183f8535211bf11829dc7ae4dee6c667c
Dezoway <kostya.trepalin.2013@mail.ru>
2022-04-06 18:05:49 +0300

    Added 1 f

--- course_project.c
+++ course_project.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<locale.h>
+#include<conio.h>
+#include<string.h>
 #define SIZE 20
 typedef struct _Node {
     char title[SIZE];
@@ -18,12 +20,13 @@ typedef struct _DLinkedList{
 
 DLinkedList* createLinkedList();//Создание списка
 void showDLList();//Просмотр списка
-void pushNode(DLinkedList *list);//Добавление в начало списка новой записи
-void findCorrect(DlinkedList *list);//Поиск и корректировка записи(узла)
-void deleteNode(DlinkedList *list);//Удаление записи(узла)
-void saveToFile(DlinkedList *list);//Сохранение списка в файл
-void loadToList(DlinkedList *list);//Загрузка списка из файла
-void exit(DlinkedList *list);//Выход
+void pushNode(DLinkedList *list);//Добавление в список новой записи
+// void sorted(DlinkedList *list);//Сортировка записей
+//void findCorrect(DlinkedList *list);//Поиск и корректировка записи(узла)
+//void deleteNode(DlinkedList *list);//Удаление записи(узла)
+//void saveToFile(DlinkedList *list);//Сохранение списка в файл
+//void loadToList(DlinkedList *list);//Загрузка списка из файла
+//void exit(DlinkedList *list);//Выход
 
 
 
@@ -31,7 +34,9 @@ void exit(DlinkedList *list);//Выход
 int main(void){
     setlocale(LC_ALL,"Rus");
     int a;
+    DLinkedList *list = NULL;
     while(1!=0){
+        system("cls");
         printf("Выберите желаемое действие:\n");
         printf("1. Создание списка.\n");
         printf("2. Просмотр списка.\n");
@@ -43,19 +48,41 @@ int main(void){
         printf("8. Выход\n");
         printf("Введите число: ");
         scanf("%d",&a);
-        printf("\n");
         switch(a){
-        case 1:NULL;break;
-        case 2:NULL;break;
-        case 3:NULL;break;
+        case 1:{
+            list = createLinkedList();
+            system("cls");
+            printf("Список создан\n");
+            system("pause");
+            break;
+        }
+        case 2:
+            system("cls");
+            if (list != NULL){
+            showDLList(list);
+            }
+            else{
+                printf("Элементы списка не обнаружены. Загрузите список из файла либо создайте список и  добавьте элементы вручную\n");
+                system("pause");
+            }
+            ;break;
+        case 3:
+            if(list){
+                system("cls");
+                pushNode(list);
+            }
+            else printf("Список не обнаружен, создайте список.");
+
+            ;break;
         case 4:NULL;break;
         case 5:NULL;break;
         case 6:NULL;break;
         case 7:NULL;break;
         case 8:return 0;
         default:
-            printf("Неверная команда\n\n");
-            getchar();
+            system("cls");
+            printf("Неверная команда\n");
+            system("pause");
             break;
         }
     }
@@ -65,13 +92,11 @@ int main(void){
 
 DLinkedList* createLinkedList(){
     DLinkedList *tmp =(DLinkedList*)malloc(sizeof(DLinkedList));
-    if(tmp == NULL)exit(1);
     tmp ->size = 0;//Указатель на размер
     tmp->head = tmp->tail = NULL;//Инициализация указателя на голову списка и хвост
+    pushNode(tmp);
     return tmp;
 }
-
-
 
 void showDLList(DLinkedList* list){
     Node *tmp = list->head;//Указатель ссылается на первый элемент
@@ -79,7 +104,48 @@ void showDLList(DLinkedList* list){
     while(tmp){
         printf("%d.%s\n%s\n%d\n\n",counter,tmp->title,tmp->type,tmp->price);
         counter++;
+        tmp = tmp->next;
     }
+    system("pause");
 }
 
+void pushNode(DLinkedList* list){
+    system("cls");
+    Node *tmp = (Node*)malloc(sizeof(Node));
+    int choice;
+    int price;
+    if(tmp == NULL){
+        printf("Нехватка памяти.");
+        exit(2);
+    }
+    printf("Введите название издания: ");
+    scanf("%s",tmp->title);
+    while(choice != 1 && choice !=2){
+        system("cls");
+        printf("Выберите тип издания:\n1. Газета\n2. Журнал\nВведите число:");
+        scanf("%d",&choice);
+        printf("%d",choice);
+        if(choice != 1 && choice != 2){
+            printf("Некорректный ввод.\n");
+            system("pause");
+        }
+        else if(choice == 1){
+            strcpy(tmp->type,"Газета");
+        }
+        else if (choice == 2){
+            strcpy(tmp->type,"Журнал");
+        }
+    }
+    system("cls");
+    printf("Введите цену: ");
+    scanf("%d",&price);
+    tmp->price = price;
+    if(list->head == NULL){
+        list->head = tmp;
+        list->tail = tmp;
+        tmp->next = NULL;
+        tmp->prev = NULL;
+    }
+
+}
 
